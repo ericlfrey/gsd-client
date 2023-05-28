200ae7d99aaf7ecbfab7a06627d2951b2f5e200b
Eric Frey <ericlfrey@gmail.com>
2023-05-28 16:32:32 -0500

    Added the taskDetails page, and edited the task details card to show the correct info.

--- components/Details/TaskDetails.js
+++ components/Details/TaskDetails.js
@@ -2,29 +2,22 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Card, Dropdown } from 'react-bootstrap';
 import Link from 'next/link';
-// import { useRouter } from 'next/router';
-// import { getSingleTask } from '../../api/taskData';
-// import { getSingleProject } from '../../api/projectData';
 import cardStyles from '../../styles/CardStyles.module.css';
 import { getSingleTask } from '../../utils/data/task_data';
-import { getSingleProject } from '../../utils/data/project_data';
-// import { deleteTaskDetails } from '../../api/mergedData';
 
-export default function TaskDetails({ firebaseKey }) {
+export default function TaskDetails({ taskId }) {
   const [task, setTask] = useState({});
-  const [project, setProject] = useState({});
 
   // const router = useRouter();
 
   useEffect(() => {
-    getSingleTask(firebaseKey).then(setTask);
-    if (task.project_id) getSingleProject(task.project_id).then(setProject);
-  }, [firebaseKey, task.project_id]);
+    getSingleTask(taskId).then(setTask);
+  }, [taskId]);
 
   const handleDeleteTask = () => {
     // if (window.confirm(`Are you sure you want to delete "${task.task_name}"? This task cannot be undone.`)) {
-    //   deleteTaskDetails(firebaseKey).then(() => {
-    //     router.push(`/project/${project.firebaseKey}`);
+    //   deleteTaskDetails(taskId).then(() => {
+    //     router.push(`/project/${project.taskId}`);
     //   });
     // }
   };
@@ -34,21 +27,21 @@ export default function TaskDetails({ firebaseKey }) {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <div className={cardStyles.cardHeader}>
-            <h3>{task.task_name}</h3>
+            <h3>{task.name}</h3>
             <Dropdown>
               <Dropdown.Toggle variant="outline-success" className={`toggle-btn ${cardStyles.cardActionsBtn}`}>
                 Actions
               </Dropdown.Toggle>
 
               <Dropdown.Menu className={cardStyles.dropdownMenu}>
-                <Link passHref href={`/task/edit/${firebaseKey}`}>
+                <Link passHref href={`/task/edit/${taskId}`}>
                   <Dropdown.Item className={cardStyles.dropdownItem}>Edit</Dropdown.Item>
                 </Link>
                 <Dropdown.Item className={cardStyles.dropdownItem} onClick={handleDeleteTask}>Delete Task</Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
           </div>
-          <footer className={`${cardStyles.cardSubtitle} blockquote-footer mt-2`}>Project: {project.title}</footer>
+          <footer className={`${cardStyles.cardSubtitle} blockquote-footer mt-2`}>Project: {task.project?.title}</footer>
           <footer className={`${cardStyles.cardSubtitle} blockquote-footer mt-2`}>Date Created: {new Date(task.date_created).toLocaleDateString()}</footer>
           <footer className={`${cardStyles.cardSubtitle} blockquote-footer mt-2`}>{task.due_date ? `Due Date: ${task.due_date}` : 'No due date'}</footer>
           <Link passHref href={`/project/${task.project_id}`}>
@@ -64,5 +57,5 @@ export default function TaskDetails({ firebaseKey }) {
 }
 
 TaskDetails.propTypes = {
-  firebaseKey: PropTypes.string.isRequired,
+  taskId: PropTypes.number.isRequired,
 };
